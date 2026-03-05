1edc62b4934205f954ce0cb7bb8e6775fa08d25b
Akhand Pratap Singh <akhandanand070707@gmail.com>
2026-03-05 14:53:42 +0530

    Add project files.

---
+++ Controllers/ComplaintController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SmartComplaintManagement.Application.Service;
+
+namespace SmartComplaintManagment.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ComplaintController : ControllerBase
+    {
+        private readonly IComplaintService _complaintService;
+
+        public ComplaintController(IComplaintService complaintService)
+        {
+            _complaintService = complaintService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateComplaint(string title, string description)
+        {
+            var result = await _complaintService.CreateComplaint(title, description);
+            return Ok(result);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+using SmartComplaintManagement.Application.Interface;
+using SmartComplaintManagement.Application.Service;
+using SmartComplaintManagement.Infrastructure.Data;
+using SmartComplaintManagement.Infrastructure.Repository;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IComplaintService, ComplaintService>();
+builder.Services.AddScoped<IComplaintRepository, ComplaintRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
